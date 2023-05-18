9f4e38d5fc4c765401a5f3292637174ad484b5f3
juanabonita2020 <129199810+juanabonita2020@users.noreply.github.com>
2023-05-18 08:51:43 -0300

    NavBar

--- resources/vue/views/NavView.vue
+++ resources/vue/views/NavView.vue
@@ -3,9 +3,10 @@ import global from './../../js/global.js';
 
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faUser } from '@fortawesome/free-regular-svg-icons'
+import { faHouse, faShirt, faInbox, faRightFromBracket } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-library.add(faUser);
+library.add(faUser,faHouse,faShirt,faInbox,faRightFromBracket);
 
 export default {
   components: {
@@ -16,6 +17,18 @@ export default {
       global,
     }
   },
+  methods: {
+    async logout(){
+      try {
+        await axios.post('/logout');
+        this.global.user = null;
+        this.$router.push({name:'login'});
+
+      } catch (error) {
+        console.log(`Error al logout: ${error.response.data.message}`);
+      }
+    },
+  },
 }
 </script>
 
@@ -23,22 +36,34 @@ export default {
   <div id="nav" class="flex flex-col transition-transform duration-500 ease-in fixed top-0 bottom-0 p-12 bg-primary text-on-primary w-[300px] rounded-tr-lg rounded-br-lg"  :class="{'-translate-x-[300px]': !global.navVisible}">
     <div class="flex-initial flex">
       <div class="flex-initial flex items-center justify-center p-3">
-        <font-awesome-icon class="text-3xl" :icon="['far', 'user']" />
+        <font-awesome-icon class="text-4xl" :icon="['far', 'user']" />
       </div>
-      <div class="p-3 text-sm flex flex-col">
+      <div class="p-3 text-md flex flex-col">
         <strong>{{ global.user.nombre }}</strong>
         <small>Sin clienta</small>
       </div>
     </div>
-    <nav class="flex-1">
-      <ul class="text-xl space-y-3 mt-12">
-        <li>Pedidos</li>
-        <li>Comunidad</li>
-        <li>Otra cosa</li>
+    <nav class="flex-initial">
+      <ul class="text-xl mt-8 space-y-5">
+        <li class="flex items-center hover:text-gray-300 cursor-pointer">
+          <font-awesome-icon class="p-2 me-2" :icon="['fas', 'house']" />
+          Home
+        </li>
+        <li class="flex items-center hover:text-gray-300 cursor-pointer">
+          <font-awesome-icon class="p-2 me-2" :icon="['fas', 'shirt']" />
+          Pedidos
+        </li>
+        <li class="flex items-center hover:text-gray-300 cursor-pointer">
+          <font-awesome-icon class="p-2 me-2" :icon="['fas', 'inbox']" />
+          Comunidad
+        </li>
       </ul>
     </nav>
-    <div class="flex-initial">
-      Salir
+    <div class="flex-initial mt-auto">
+      <button @click="logout" class="flex items-center hover:text-gray-300 cursor-pointer text-xl">
+        <font-awesome-icon class="p-2 me-2" :icon="['fas', 'right-from-bracket']" />
+        Salir
+      </button>
     </div>
   </div>
 </template>
